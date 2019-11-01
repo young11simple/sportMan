7438d32be53493bfa211d1a3d4ba20377fe039a2
NANA <nanawllin@outlook.com>
2019-11-01 12:12:07 +0800

    dist1101

--- vue.config.js
+++ vue.config.js
@@ -99,18 +99,18 @@ module.exports = {
   },
 
   // development server port 9999,
-  devServer: {
-    port: 9009,
-    proxy: {
-      '/api': {
-        // target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-        target: 'http://localhost:8088',
-        // target: 'http://172.16.0.15:8088/',
-        ws: false,
-        changeOrigin: true
-      }
-    }
-  },
+  // devServer: {
+  //   port: 9009,
+  //   proxy: {
+  //     '/api': {
+  //       // target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+  //       target: 'http://localhost:8088',
+  //       // target: 'http://172.16.0.15:8088/',
+  //       ws: false,
+  //       changeOrigin: true
+  //     }
+  //   }
+  // },
 
   lintOnSave: undefined,
   // babel-loader no-ignore node_modules/*
